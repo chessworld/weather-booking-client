ea67effcdb1fc1e7c06203dfff955fbaad7ddb35
Luyang Liu <luyangl16@gmail.com>
2023-04-30 18:44:16 +1000

    Improved slider design.

--- weather-booking-app-client/src/components/Screen/Background.tsx
+++ weather-booking-app-client/src/components/Screen/Background.tsx
@@ -13,7 +13,7 @@ class Background extends React.Component<AbcProps, {}> {
 
     render() {
         return (
-            <IonContent>
+            <IonContent className="ion-no-padding">
                 {
                     this.props.children
                 }
