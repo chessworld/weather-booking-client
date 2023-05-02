c4dce58cb378175adf4a65ae6175d6d643c495b4
Luyang Liu <luyangl16@gmail.com>
2023-05-02 22:41:27 +1000

    Positioned card child elements for home page and added chevronBackOutline to a page.

--- weather-booking-app-client/src/components/BookingDetails.tsx
+++ weather-booking-app-client/src/components/BookingDetails.tsx
@@ -1,32 +1,33 @@
 import { useParams } from "react-router-dom";
 import { IonButton, IonIcon } from "@ionic/react";
+import { chevronBackOutline } from 'ionicons/icons';
 
 type map = {
-  id: number;
-  location: string;
-  date: string;
-  weather: string;
+    id: number;
+    location: string;
+    date: string;
+    weather: string;
 };
 
 interface IWeatherCardList {
-  closeBookingDetail: (booking: any) => void;
-  data: map;
+    closeBookingDetail: (booking: any) => void;
+    data: map;
 }
 
 const BookingDetails: React.FC<IWeatherCardList> = (props) => {
-  console.log(props);
+    console.log(props);
 
-  return (
-    <div>
-      <IonButton onClick={() => props.closeBookingDetail(null)}>
-        <IonIcon slot="icon-only" name="chevron-back-outline"></IonIcon>
-      </IonButton>
-      <h1>Booking Details</h1>
-      <h2>{props.data.location}</h2>
-      <h2>{props.data.date}</h2>
-      <h2>{props.data.weather}</h2>
-    </div>
-  );
+    return (
+        <div>
+            <IonButton onClick={() => props.closeBookingDetail(null)}>
+                <IonIcon name={chevronBackOutline}></IonIcon>
+            </IonButton>
+            <h1>Booking Details</h1>
+            <h2>{props.data.location}</h2>
+            <h2>{props.data.date}</h2>
+            <h2>{props.data.weather}</h2>
+        </div>
+    );
 };
 
 export default BookingDetails;
--- weather-booking-app-client/src/components/WeatherCardList.tsx
+++ weather-booking-app-client/src/components/WeatherCardList.tsx
@@ -1,55 +1,63 @@
 import {
-  IonList,
-  IonCardHeader,
-  IonItem,
-  IonCardContent,
-  IonCardTitle,
-  IonCardSubtitle,
-  IonCard,
-  IonImg,
-  IonRouterOutlet,
+    IonList,
+    IonItem,
+    IonCard,
+    IonImg,
 } from "@ionic/react";
+
+import { useState, useEffect } from "react";
 import "./WeatherCardList.css";
 import sunImage from "../assets/Icons/Sun.png";
+import arrowRight from "../assets/Icons/arrow-right.png";
+import React from 'react';
 import BookingDetails from "./BookingDetails";
-import { Route } from "react-router";
-import { Link } from "react-router-dom";
 
 type map = {
-  id: number;
-  location: string;
-  date: string;
-  weather: string;
+    id: number;
+    location: string;
+    date: string;
+    weather: string;
 };
 
 interface IWeatherCardList {
-  openBookingDetail: (booking: any) => void;
-  data: map[];
+    openBookingDetail: (booking: any) => void;
+    data: map[];
 }
 
 const WeatherCardList: React.FC<IWeatherCardList> = (props) => {
-  return (
-    <div>
-      <IonList>
-        <div className="cardContainer">
-          {props.data.map(({ id, location, date, weather }: map) => (
-            <IonItem key={id} onClick={() => props.openBookingDetail(id)}>
-              <IonCard className="weatherBookingCard">
-                <IonCardHeader>
-                  <div className="imageContainer">
-                    <IonImg src={sunImage} />
-                  </div>
-                  <IonCardTitle>{location}</IonCardTitle>
-                  <IonCardSubtitle>{date}</IonCardSubtitle>
-                </IonCardHeader>
-                <IonCardContent>{weather}</IonCardContent>
-              </IonCard>
-            </IonItem>
-          ))}
+    // TODO place all card list and card info in useState and update state with API calls.
+    // TODO could try https://builtin.com/software-engineering-perspectives/use-query-react useQuery to call API.
+    
+    return (
+        <div>
+            <IonList>
+                <div className="cardContainer">
+                    {
+                        props.data.map(({ id, location, date, weather }: map) => (
+                            <IonItem key={id} onClick={() => props.openBookingDetail(id)}>
+                                <IonCard className="weather-booking-card">
+                                    <div className="container-root">
+                                        <div className="container-group-right">
+                                            <div className="imageContainer">
+                                                <IonImg className="card-weather-image" src={sunImage} />
+                                            </div>
+                                            <div className="container-group-text">
+                                                <h1 className="card-title">{location}</h1>
+                                                <p className="card-subtitle">{date}</p>
+                                                <p className="card-text">{weather}</p>
+                                            </div>
+                                        </div>
+
+                                        <IonImg className="card-arrow-image" src={arrowRight} />
+                                    </div>
+                                </IonCard>
+                            </IonItem>
+                        ))
+                    }
+                </div>
+            </IonList>
         </div>
-      </IonList>
-    </div>
-  );
+    );
 };
 
 export default WeatherCardList;
