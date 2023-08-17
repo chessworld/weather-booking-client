8e0cf5c94110dad962a22c87d53900dc06366a43
Dale1201 <dben0010@student.monash.edu>
2023-08-17 20:06:32 +1000

    fix wrong time period values bug

--- weather-booking-app-client/src/endpoint-caller/bookingEndpoint.tsx
+++ weather-booking-app-client/src/endpoint-caller/bookingEndpoint.tsx
@@ -2,6 +2,7 @@ import ApiService from "./apiService";
 import { BookingResponse } from "./interfaces/bookings/BookingResponse";
 import { WeatherOption } from "./interfaces/bookings/WeatherOption";
 import { Location } from "./interfaces/locations/Location";
+import { TimePeriod } from "./interfaces/enums/TimePeriod";
 
 export default class BookingEndpoint {
   static async getBookingList(userId: string): Promise<BookingResponse[]> {
@@ -16,11 +17,11 @@ export default class BookingEndpoint {
 
   static createBooking = (
     userId: string,
-    location: number, date: string,
-    timePeriod: string,
+    location: number,
+    date: string,
+    timePeriod: TimePeriod,
     weatherOption: WeatherOption
   ) => {
-
     const body = {
       location: location,
       date: date,
@@ -28,7 +29,7 @@ export default class BookingEndpoint {
       weather_option: weatherOption,
     };
 
-    console.log(JSON.stringify( body ));
+    console.log(JSON.stringify(body));
     ApiService.post(`/bookings/user/${userId}/`, { ...body });
   };
 }
